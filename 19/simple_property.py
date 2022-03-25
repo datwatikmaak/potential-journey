40378e58bcac39223c8709221994b3214f20aae9
Leonieke <datwatikmaak@gmail.com>
2022-03-25 03:42:18 +0100

    submission Bite 19 @ codechalleng.es

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -1,7 +1,14 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 NOW = datetime.now()
 
 
 class Promo:
-    pass
+    def __init__(self, name, expires):
+        self.name = name
+        self.expires = expires
+
+    @property
+    def expired(self):
+        if (NOW - self.expires) > timedelta(days=0):
+            return True

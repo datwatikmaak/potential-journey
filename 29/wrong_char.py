dd788ad243e12aa30d23258bb3d038c50f7e01dc
Leonieke <datwatikmaak@gmail.com>
2022-03-27 19:40:55 +0200

    submission Bite 29 @ codechalleng.es

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,16 @@
 def get_index_different_char(chars):
-    pass
+    enumerate_chars_dict = dict(list(enumerate(chars)))
+
+    alnum_char = []
+    non_alnum_char = []
+
+    for key, value in enumerate_chars_dict.items():
+        if str(value).isalpha() or str(value).isdigit():
+            alnum_char.append(key)
+        else:
+            non_alnum_char.append(key)
+
+    if len(alnum_char) > len(non_alnum_char):
+        return non_alnum_char[0]
+    if len(alnum_char) < len(non_alnum_char):
+        return alnum_char[0]
